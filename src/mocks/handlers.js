3a3bdde865756d62b18c4cfc2ed0b24d48472b25
Svetozar Stanev <svetozar.stanev@gmail.com>
2023-03-05 00:21:47 +0200

    added <AppHeading /> tests

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,17 +1,8 @@
 // src/mocks/handlers.js
 import { rest } from "msw";
+import { allUsers } from "./allUsers";
 
 const baseUrl = "https://jsonplaceholder.typicode.com";
-
-const allUsers = [
-  {
-    id: 999,
-    name: "Test Name",
-    username: "Test Username",
-    email: "email@test.com",
-    phone: "99999999999",
-  },
-];
 
 export const handlers = [
   rest.get(`${baseUrl}/users`, (req, res, ctx) => {
